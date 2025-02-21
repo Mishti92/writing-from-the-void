bbdc872825beaaed1d5ae763390fbf5d191b3ada
Mansha <manshaimtiyaz@Winter-Mac.local>
2025-02-21 17:07:22 -0300

    Quartz sync: Feb 21, 2025, 5:07 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Shae's Journal",
+    pageTitle: "📕 Shae's Journal",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
